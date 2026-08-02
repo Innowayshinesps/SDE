f76fa7137bb622c076f9ff9fd8e68bfa93244396
Surya Pratap Singh <suryapratapsinghsrineth@gmail.com>
2026-08-02 23:18:02 +0530

    Javascript DAY-3 Phase-2

---
+++ JS/JS_Day2_Phase2/script.js
@@ -0,0 +1,22 @@
+// Js is wrapped with webAPI and it has given DOM, Location, Console, Alert, Confirm, Error, History, localStorage, Fetch apis and all
+
+//Var is a global variable, it gets with WebAPI window object. That's a downside
+
+//Var and let allows u to reassign variables whereas const doesn't allows because its a constant variable.
+
+//Var is a function scope whereas let and const is a block scope
+
+// Strings:- Concatination
+// Use '', "" or template literals which is `(Backtick)`
+// var num = 10    
+// var a = 'Hi Browserstack'
+// console.log(a + num)
+
+// In order to remove the recurring quotes in a log statement we can use backtick and ${variable}
+
+// var num = 10    
+// var a = 'Hi Browserstack,'
+
+// console.log(`${a} I am ${num} years old`)
+
+// Strings are immutable 
